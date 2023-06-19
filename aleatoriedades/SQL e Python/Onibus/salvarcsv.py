bb4f113e6de59d11469db5debc01d60d71163ac8
Ana Coelho <70607969+anabcoelho@users.noreply.github.com>
2023-06-18 23:36:37 -0300

    Delete aleatoriedades/SQL e Python/Onibus directory

--- aleatoriedades/SQL e Python/Onibus/salvarcsv.py
+++
@@ -1,9 +0,0 @@
-def salvar_csv (classe, results, headers):
-    import csv
-
-    with open(rf'{classe}.csv', 'w') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',', lineterminator='\r',
-                            quoting=csv.QUOTE_ALL, escapechar='\\')
-        writer.writerow(headers)
-        writer.writerows(results)
-    print('salvo')
