0f2556d907f00228f580fab684478dafbe3397f4
Ana Coelho <70607969+anabcoelho@users.noreply.github.com>
2023-06-18 23:30:31 -0300

    Update conectar.py

--- aleatoriedades/SQL e Python/Onibus/conectar.py
+++ aleatoriedades/SQL e Python/Onibus/conectar.py
@@ -1,5 +1,4 @@
-#Agora que você criou as tabelas, comece a desenvolver o seu sistema em Python levando em consideração as orientações acima para classes e suas entidades e a conexão com o banco
-#crie uma interface no terminal para que o usuário possa cadastrar e visualizar as informações
+
 def conectar():
     import pyodbc
     import dados_conexao #por segurança todos os dados de conexão estão em um arquivo dados_conexão.py
